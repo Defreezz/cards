fde44bef048b03d2cc3a026871d177d79b5146b0
Arman Alimov <527c@mail.ru>
2022-03-10 11:58:59 +0600

    add structure and useState for pass, email

--- src/main/ui/LogIn/LogIn.tsx
+++ src/main/ui/LogIn/LogIn.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const LogIn = () => {
-  return <div>Log in</div>;
+
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  return (
+      <div>
+        <div>
+            Sign in
+        </div>
+        <div>
+            Email
+        </div>
+        <div>
+            <input value={email} onChange={ (e) => setEmail(e.target.value)}/>
+        </div>
+        <div>
+            Password
+        </div>
+        <div>
+            <input value={password} onChange={(e) => setPassword(e.target.value)}/>
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+          {email}
+      </div>
+  );
 };
