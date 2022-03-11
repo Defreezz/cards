968a818ba985caa261cce41266c13635dfc7518b
Artsiom <yatsevich-artsiom@yandex.by>
2022-03-11 15:14:57 +0300

    merge with arman

--- src/main/components/LogIn/LogIn.tsx
+++ src/main/components/LogIn/LogIn.tsx
@@ -1,14 +1,9 @@
 import React, {useState} from "react";
 import {getLoginUserData} from "../../../store/reducers/loginReducer";
-import {useDispatch, useSelector} from "react-redux";
-import style from "./LogIn.module.css"
+import {useDispatch} from "react-redux";
 import SuperInputText from "../common/SuperInputText/SuperInputText";
 import SuperButton from "../common/SuperButton/SuperButton";
 import SuperCheckbox from "../common/SuperCheckbox/SuperCheckbox";
-import {AppStoreType} from "../../../store/store";
-import { Navigate } from "react-router-dom";
-import { Path } from "../Routes/Router";
-import {useDispatch} from "react-redux";
 import {NavLink} from "react-router-dom";
 import {Path} from "../Routes/Router";
 
@@ -18,15 +13,12 @@ export const LogIn = () => {
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
     const [checked, setChecked] = useState<boolean>(false)
-    const isLogin = useSelector<AppStoreType, boolean>((state => state.logIn.isLogin))
     const onClickButtonLogin = () => {
         dispatch(getLoginUserData(email, password, checked))
         setEmail('')
         setPassword('')
         setChecked(false)
     }
-
-    if (isLogin) return <Navigate to={Path.Profile}/>
 
     return (
         <div>
